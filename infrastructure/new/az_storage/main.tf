31bf146205375d83a1438c85d589b235c58cee5c
stephanesbizzera <stephanesbizzera@kpmg.com>
2020-12-09 17:06:41 -0500

    update

---
+++ infrastructure/new/az_storage/main.tf
@@ -0,0 +1,23 @@
+resource "azurerm_storage_account" "example" {
+  name                     = "${random_string.prefix2.id}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_share" "example" {
+  name                 = "${random_string.prefix2.id}"
+  storage_account_name = azurerm_storage_account.example.name
+  quota                = 50
+
+  acl {
+    id = "2b2ee77d-8520-4459-a035-26afd4fb051e"
+
+    access_policy {
+      permissions = "rwdl"
+      start       = "2019-07-02T09:38:21.0000000Z"
+      expiry      = "2019-07-02T10:38:21.0000000Z"
+    }
+  }
+}
--- local/airflow_docker/test.py
+++ local/airflow_docker/test.py
@@ -1,28 +1,29 @@
-from airflow.providers.microsoft.azure.sensors.wasb import WasbBlobSensor
-from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
+# from airflow.providers.microsoft.azure.sensors.wasb import WasbBlobSensor
+# from airflow.providers.microsoft.azure.hooks.wasb import WasbHook
 
 # from airflow.contrib.hooks.wasb_hook import WasbHook
 # from airflow.contrib.sensors.wasb_sensor import WasbBlobSensor
 
-from airflow import settings
-from airflow.models import Connection
+# from airflow import settings
+# from airflow.models import Connection
 
-account_name = 'terraformaccount2'
+# account_name = 'terraformaccount2'
 key = 'R+7/s6BFH+O+qCmdOfRrBdls94+VqkA51KwT/xOig5Hsl+LiHZJ7hv8zN36InVhWfNf46g5YVlNcpB6JtqUZRQ=='
-conn_id='wasb_default'
+# conn_id='wasb_default'
 
-conn = Connection(
-        conn_id=conn_id,
-        login=account_name,
-        password=key,
-) #create a connection object
-session = settings.Session() # get the session
-session.add(conn)
-
-session.commit() # it will insert the connection object programmatically.
-hook = WasbHook(wasb_conn_id='wasb_default')
-AZURE_CONTAINER_NAME = 'tfstate'
-blob_name ='ls.csv'
-file_path = '../'
-connection_string = 'DefaultEndpointsProtocol=https;AccountName=terraformaccount2;AccountKey=R+7/s6BFH+O+qCmdOfRrBdls94+VqkA51KwT/xOig5Hsl+LiHZJ7hv8zN36InVhWfNf46g5YVlNcpB6JtqUZRQ==;EndpointSuffix=core.windows.net'
+# c = Connection(
+#          conn_type='wasb_default',
+#         conn_id=conn_id,
+#         login=account_name,
+#         password=key,
+# ) #create a connection object
+# session = settings.Session() # get the session
+# session.add(c)
+# print(f"AIRFLOW_CONN_{c.conn_id.upper()}='{c.get_uri()}'")
+# session.commit() # it will insert the connection object programmatically.
+# hook = WasbHook(wasb_conn_id='wasb_default')
+# AZURE_CONTAINER_NAME = 'tfstate'
+# blob_name ='ls.csv'
+# file_path = '../'
+# connection_string = 'DefaultEndpointsProtocol=https;AccountName=terraformaccount2;AccountKey=R+7/s6BFH+O+qCmdOfRrBdls94+VqkA51KwT/xOig5Hsl+LiHZJ7hv8zN36InVhWfNf46g5YVlNcpB6JtqUZRQ==;EndpointSuffix=core.windows.net'
 # hook.get_file(file_path, AZURE_CONTAINER_NAME, blob_name)
